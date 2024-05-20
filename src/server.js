cddea26e86908f0f6a15c893014b4205d8952163
fajrCode <fajri42vandi@gmail.com>
2024-05-20 14:20:04 +0700

    fefactor

--- src/server.js
+++ src/server.js
@@ -1,62 +1,18 @@
-import "dotenv/config"
-import express from 'express'
-import OpenAI from 'openai'
+import 'dotenv/config';
+// import http from 'http';
+import app from './app.js';
 
-const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+// const server = http.createServer(app);
 
-app.use(express.static('public'))
-
-const openai = new OpenAI({
-    apiKey: process.env.KEY,
-});
-
-// SSE route
-app.get('/sse', async (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`🚀 [SERVER] is running on port http://localhost:${PORT}`);
     });
-    const sendData = (data) => {
-        // res.write(`data: ${JSON.stringify(data)}\n\n`);
-        res.write(`data: ${data}\n\n`);
-    };
+  } catch (error) {
+    console.log(`⚠️ [ERROR], ${error}`);
+  }
+};
 
-    // hit api;
-    const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            { "role": "system", "content": "You are a helpful assistant." },
-            { "role": "user", "content": "Hello!" }
-        ],
-        stream: true,
-    });
-
-    //hit result api
-    try {
-        for await (const chunk of completion) {
-            const result = chunk.choices[0].delta.content;
-            if (result) {
-                sendData(result)
-            }
-        }
-
-    } catch (error) {
-        console.log(error)
-    }
-
-    // Simulate SSE data
-    // const intervalId = setInterval(async () => {
-    //     console.log(kata)
-    //     sendData(kata);
-    // }, 1000);
-    // // Close SSE connection when the client disconnects
-    // req.on('close', () => {
-    //     clearInterval(intervalId);
-    // });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+start();
